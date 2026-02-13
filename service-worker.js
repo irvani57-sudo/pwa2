f89bf8f49ac0775d775175f0f13d65940d25c15f
irvani57-sudo <irvani57@gmail.com>
2026-02-13 20:49:33 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "alfredo-v2";
+const CACHE_NAME = "omidepay-cache-v1";
 
 const FILES_TO_CACHE = [
   "./",
@@ -34,8 +34,8 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
