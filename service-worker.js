21adb173eab8298319af7d22586a46a480879ecb
irvani57-sudo <irvani57@gmail.com>
2026-02-13 16:44:57 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
 const CACHE_NAME = 'omidepay-cache-v1';
+
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icon/icon-192.png',
+  './icon/icon-512.png'
 ];
 
-// نصب و کش کردن فایل‌ها
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-// فعال‌سازی (پاک‌سازی کش‌های قدیمی)
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -24,11 +24,13 @@ self.addEventListener('activate', event => {
       )
     )
   );
+  self.clients.claim();
 });
 
-// هندل درخواست‌ها — اول کش، بعد شبکه
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
   );
 });
