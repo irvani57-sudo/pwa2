f863ee786ba92e00e3146ee3c3c25b190e13c873
irvani57-sudo <irvani57@gmail.com>
2026-02-13 20:35:57 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
+{
+  "name": "Alfredo_Omidpay",
+  "short_name": "Alfredo",
+  "start_url": "/",
+  "scope": "/",
+  "display": "standalone",
+  "theme_color": "#1976d2",
+  "background_color": "#ffffff"
+}
